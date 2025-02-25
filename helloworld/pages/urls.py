4ca54e1852e7d9c36cac1ae171642f99c8512b2b
Juan Zapata <juadzm215@gmail.com>
2025-02-25 14:44:50 -0500

    tutorial 3

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .utils import ImageLocalStorage
 from .views import *
 
 urlpatterns =[
@@ -8,5 +9,10 @@ urlpatterns =[
     path('products/',ProductIndexView.as_view(),name='index'),
     path('products/create/',ProductCreateView.as_view(),name='form'),
     path('products/create/created/',ProductCreatedView.as_view(),name='created'),
-    path('products/<str:id>',ProductShowView.as_view(),name='show'), 
-]   
+    path('products/<str:id>',ProductShowView.as_view(),name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -12,43 +12,55 @@ from .models import Product
 #Nota:
 #TemplateView: GET y POST implementados. Útil para renderizar plantillas sin más.
 #View: Hay que incluir GET y POST. Flexibilidad en la lógica de estos métodos.
+def ImageViewFactory(image_storage):
+ class ImageView(View):
+    template_name = 'images/index.html'
 
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+        return redirect('image_index')
+ return ImageView
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
-class AboutPageView(TemplateView): 
-    template_name = 'pages/about.html' 
-     
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
-        context.update({ 
-            "title": "About us - Online Store", 
-            "subtitle": "About us", 
-            "description": "This is an about page ...", 
-            "author": "Developed by: Juan Miguel", 
-        }) 
- 
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Juan Miguel",
+        })
+
         return context
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
- 
-class ProductIndexView(View): 
-    template_name = 'products/index.html' 
- 
-    def get(self, request): 
-        viewData = {} 
-        viewData["title"] = "Products - Online Store" 
-        viewData["subtitle"] =  "List of products" 
-        viewData["products"] = Product.objects.all() 
- 
-        return render(request, self.template_name, viewData) 
- 
-class ProductShowView(View): 
-    template_name = 'products/show.html' 
- 
- 
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] =  "List of products"
+        viewData["products"] = Product.objects.all()
+
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+
     def get(self, request, id):
 
         try:
@@ -56,18 +68,18 @@ class ProductShowView(View):
             if product_id < 1:
                 raise ValueError("Product id must be 1 or greater")
             product = get_object_or_404(Product,pk=product_id)
-        
+
         except (IndexError, ValueError):
             return HttpResponseRedirect(reverse('home')) #Si el id de la URL no existe vuelve a home
-        
+
         viewData = {}
-        product = get_object_or_404(Product,pk=product_id)  
-        viewData["title"] = product.name + " - Online Store" 
-        viewData["subtitle"] =  product.name + " - Product information" 
+        product = get_object_or_404(Product,pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] =  product.name + " - Product information"
         viewData["product"] = product
 
         return render(request, self.template_name, viewData)
-    
+
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
@@ -79,10 +91,10 @@ class ProductListView(ListView):
         context['subtitle'] = 'List of products'
 
         return context
-        
 
-class ProductForm(forms.ModelForm): 
-    
+
+class ProductForm(forms.ModelForm):
+
     class Meta:
         model = Product
         fields = ['name','price']
@@ -92,27 +104,68 @@ class ProductForm(forms.ModelForm):
         if price <= 0:
             raise forms.ValidationError('The price must be greater than zero.')
         return price
- 
-class ProductCreateView(View): 
-    template_name = 'products/create.html' 
- 
-    def get(self, request): 
-        form = ProductForm() 
-        viewData = {} 
-        viewData["title"] = "Create product" 
-        viewData["form"] = form 
-        return render(request, self.template_name, viewData) 
- 
-    def post(self, request): 
-        form = ProductForm(request.POST) 
-        if form.is_valid(): 
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
             form.save()
-            return redirect('created')  
-        else: 
-            viewData = {} 
-            viewData["title"] = "Create product" 
-            viewData["form"] = form 
+            return redirect('created')
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
             return render(request, self.template_name, viewData)
 
 class ProductCreatedView(TemplateView):
     template_name = 'products/created.html'
+
+class CartView(View):
+ template_name = 'cart/index.html'
+
+ def get(self, request):
+ # Simulated database for products
+    products = {}
+    products[121] = {'name': 'Tv samsung', 'price': '1000'}
+    products[11] = {'name': 'Iphone', 'price': '2000'}
+    # Get cart products from session
+    cart_products = {}
+    cart_product_data = request.session.get('cart_product_data', {})
+    for key, product in products.items():
+
+        if str(key) in cart_product_data.keys():
+            cart_products[key] = product
+
+    # Prepare data for the view
+    view_data = {
+    'title': 'Cart - Online Store',
+    'subtitle': 'Shopping Cart',
+    'products': products,
+    'cart_products': cart_products
+    }
+
+    return render(request, self.template_name, view_data)
+
+ def post(self, request, product_id):
+    # Get cart products from session and add the new product
+    cart_product_data = request.session.get('cart_product_data', {})
+    cart_product_data[product_id] = product_id
+    request.session['cart_product_data'] = cart_product_data
+    return redirect('cart_index')
+
+class CartRemoveAllView(View):
+ def post(self, request):
+    # Remove all products from cart in session
+    if 'cart_product_data' in request.session:
+        del request.session['cart_product_data']
+    return redirect('cart_index')
+
